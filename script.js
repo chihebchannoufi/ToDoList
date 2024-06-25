088792e5694686457c0de103f4b3edc7b2f0b389
chihebchannoufi <channoufichiheb@gmail.com>
2024-06-25 11:58:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+const todoList = document.getElementById("todoList");
+const newTodoInput = document.getElementById("newToDoInput");
+const addTodoBtn = document.getElementById("addTodoBtn");
+
+
+addTodoBtn.addEventListener("click",()=>{
+
+    const newTodoText = newTodoInput.value;
+    if(newTodoText!==""){
+        const newTodoItem = document.createElement("li");
+        newTodoItem.innerHTML = newTodoText;
+        const deleteTodoBtn = document.createElement("button");
+        deleteTodoBtn.innerHTML = "X";
+
+        deleteTodoBtn.classList.add("delete-todo-btn");
+        deleteTodoBtn.addEventListener("click",function(){
+            newTodoItem.remove();
+        });
+        newTodoItem.appendChild(deleteTodoBtn);
+        todoList.appendChild(newTodoItem);
+        newTodoInput.value = "";
+    }
+});
+});
